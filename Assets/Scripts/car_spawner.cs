e8384c4a07b2730990264b51af068852c640c3f5
Kavyansh Barjattya <117171893+kavyanshbarjattya@users.noreply.github.com>
2023-06-12 21:26:18 +0530

    updated random car spawing system

--- Assets/Scripts/car_spawner.cs
+++ Assets/Scripts/car_spawner.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class car_spawner : MonoBehaviour
 {
-    [SerializeField] private GameObject car;
+    [SerializeField] private GameObject[] car;
     [SerializeField] private float destroySpeed;
-    [SerializeField] private float xPos;
+    [SerializeField] private int xPos;
+    [SerializeField] private Canvas normalcar_screen;
+    [SerializeField] private Canvas powercar_screen;
+    [SerializeField] private TextMeshProUGUI normal_car_score;
+    [SerializeField] private TextMeshProUGUI power_car_score;
+
+    private int normal_scoreHolder = 0;
+    private int power_scoreHolder = 0;
     void Start()
     {
         StartCoroutine(CarWaiter());
+        normalcar_screen.enabled = false;
+        powercar_screen.enabled = false;
     }
     private void Update()
     {
@@ -17,14 +27,32 @@ public class car_spawner : MonoBehaviour
         {
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+            // This is for normal car
             if (Physics.Raycast(ray, out hit))
             {
-                if (hit.collider.tag == "Car")
+                if (hit.collider.tag == "Normal Car")
                 {
-                    Debug.Log("Pressed");
+                    normal_scoreHolder+= 10;
+                    normal_car_score.text = "Normal Car : " + normal_scoreHolder;
                     Destroy(hit.transform.gameObject);
+                    normalcar_screen.enabled = true;
                 }
             }
+
+            // This is for power car
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "Power Car")
+                {
+                    power_scoreHolder+= 20;
+                    power_car_score.text = "Power Car : " + power_scoreHolder;
+
+                    Destroy(hit.transform.gameObject);
+                    powercar_screen.enabled = true;
+                }
+            }
+
+            // This is for fast car
         }
         else if (Input.touchCount > 0)
         {
@@ -37,16 +65,14 @@ public class car_spawner : MonoBehaviour
                     Destroy(hit.transform.gameObject);
                 }
             }
-
         }
     }
-
     IEnumerator CarWaiter() // this will apply waiting time to car spawner
     {
-        while (car)
+        while (car[0])
         {
-            xPos = Random.Range(-30f, 31f);
-            GameObject carclone = Instantiate(car, new Vector3(xPos ,transform.position.y,transform.position.z), Quaternion.identity);
+            xPos = Random.Range(-30, 31);
+            GameObject carclone = Instantiate(car[Random.Range(0,2)], new Vector3(xPos ,transform.position.y,transform.position.z), Quaternion.identity);
             yield return new WaitForSeconds(2f);
             Destroy(carclone , destroySpeed);
         }
