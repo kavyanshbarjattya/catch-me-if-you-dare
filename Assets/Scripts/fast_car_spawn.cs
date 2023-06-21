6a59f2fce797619d97b683a634b64ea743649cc9
Kavyansh Barjattya <117171893+kavyanshbarjattya@users.noreply.github.com>
2023-06-21 19:56:10 +0530

    updated level

--- Assets/Scripts/fast_car_spawn.cs
+++ Assets/Scripts/fast_car_spawn.cs
@@ -1,14 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
-public class car_spawner : MonoBehaviour
+public class fast_car_spawn : MonoBehaviour
 {
     [Header("---------------SerializedFields---------------")]
     [SerializeField] private GameObject car;
     [SerializeField] private float destroySpeed;
-    [SerializeField] private Transform normalcar_Trans;
+    [SerializeField] private Transform fastcar_Trans;
     [SerializeField] private int spawntimer;
 
     private GameObject carclone;
@@ -19,15 +18,15 @@ public class car_spawner : MonoBehaviour
     }
     private void Update()
     {
-        
+
         if (Application.isEditor && Input.GetMouseButtonDown(0))
         {
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            // This is for normal car
+            // This is for fast car
             if (Physics.Raycast(ray, out hit))
             {
-                if (hit.collider.tag == "Normal Car")
+                if (hit.collider.tag == "Fast Car")
                 {
                     Destroy(hit.transform.gameObject);
                 }
@@ -38,7 +37,7 @@ public class car_spawner : MonoBehaviour
         {
             var ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
             RaycastHit hit;
-            if (Physics.Raycast(ray,out hit))
+            if (Physics.Raycast(ray, out hit))
             {
                 if (hit.collider.tag == "Car")
                 {
@@ -46,16 +45,16 @@ public class car_spawner : MonoBehaviour
                 }
             }
         }
- 
+
     }
     IEnumerator CarWaiter() // this will apply waiting time to car spawner
     {
         while (car)
         {
-            carclone = Instantiate(car,normalcar_Trans.position , Quaternion.identity);
-            spawntimer = Random.Range(1, 3);
-            yield return new WaitForSeconds(spawntimer) ;
-            Destroy(carclone , destroySpeed);
+            carclone = Instantiate(car, fastcar_Trans.position, Quaternion.identity);
+            spawntimer = Random.Range(1, 7);
+            yield return new WaitForSeconds(spawntimer);
+            Destroy(carclone, destroySpeed);
         }
     }
 
--- Assets/Scripts/power_Car.cs
+++ Assets/Scripts/power_Car.cs
@@ -3,33 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class car_spawner : MonoBehaviour
+public class power_Car : MonoBehaviour
 {
     [Header("---------------SerializedFields---------------")]
     [SerializeField] private GameObject car;
     [SerializeField] private float destroySpeed;
-    [SerializeField] private Transform normalcar_Trans;
-    [SerializeField] private int spawntimer;
+    [SerializeField] private Transform powercar_Trans;
+    [SerializeField] private int spawnTimer;
 
+    [Header("---------------Public class---------------")]
+    public int powercar_reseter; // this will reset the powercar health
+    public int powercar_damage = 100;
+
+    private int power_carhealth;
     private GameObject carclone;
 
     void Start()
     {
         StartCoroutine(CarWaiter());
+        power_carhealth = powercar_reseter;
     }
     private void Update()
     {
-        
+
         if (Application.isEditor && Input.GetMouseButtonDown(0))
         {
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            // This is for normal car
+
+            // This is for power car
             if (Physics.Raycast(ray, out hit))
             {
-                if (hit.collider.tag == "Normal Car")
+                if (hit.collider.tag == "Power Car")
                 {
-                    Destroy(hit.transform.gameObject);
+                    // this is the power_car health :
+                    power_carhealth -= powercar_damage;
+                    if (power_carhealth == 0)
+                    {
+                        Destroy(hit.transform.gameObject);
+                        power_carhealth = powercar_reseter;
+                    }
                 }
             }
 
@@ -38,7 +51,7 @@ public class car_spawner : MonoBehaviour
         {
             var ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
             RaycastHit hit;
-            if (Physics.Raycast(ray,out hit))
+            if (Physics.Raycast(ray, out hit))
             {
                 if (hit.collider.tag == "Car")
                 {
@@ -46,16 +59,16 @@ public class car_spawner : MonoBehaviour
                 }
             }
         }
- 
+
     }
     IEnumerator CarWaiter() // this will apply waiting time to car spawner
     {
         while (car)
         {
-            carclone = Instantiate(car,normalcar_Trans.position , Quaternion.identity);
-            spawntimer = Random.Range(1, 3);
-            yield return new WaitForSeconds(spawntimer) ;
-            Destroy(carclone , destroySpeed);
+            carclone = Instantiate(car, powercar_Trans.position, Quaternion.identity);
+            spawnTimer = Random.Range(1, 10);
+            yield return new WaitForSeconds(spawnTimer);
+            Destroy(carclone, destroySpeed);
         }
     }
 
